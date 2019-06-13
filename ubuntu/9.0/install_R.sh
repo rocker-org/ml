02d619e9498cc83420eca22fc852f6f5800e5c3b
Carl Boettiger <cboettig@gmail.com>
2019-06-13 12:37:24 -0700

    locale

--- ubuntu/9.0/install_R.sh
+++ ubuntu/9.0/install_R.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
-
+set -e
 
 echo "deb https://cloud.r-project.org/bin/linux/ubuntu ${UBUNTU_VERSION}-cran35/" >> /etc/apt/sources.list && \
     gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9 && \
     gpg -a --export E298A3A825C0D65DFD57CBB651716619E084DAB9 | apt-key add - && \
-    apt-get update && apt-get -y install r-base && \
+    apt-get update && \
+    apt-get -y install --no-install-recommends \
+      ca-certificates \
+      less \
+      locales \
+      r-base \
+      vim-tiny \
+      wget && \
     rm -rf /var/lib/apt/lists/*
+
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen && \
+  locale-gen en_US.utf8 && \
+  /usr/sbin/update-locale LANG=${LANG}
 
 
