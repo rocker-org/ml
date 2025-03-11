b76d4e86b08dc3f5445b6d8fa054a26a6552c8bd
Carl Boettiger <cboettig@gmail.com>
2025-03-11 10:51:31 -0700

    tidy

--- install_utilities.sh
+++ install_utilities.sh
@@ -11,7 +11,8 @@ mkdir /opt/share && chown -R ${NB_USER}:users /opt/share
 # awscli tool for S3 use
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
      unzip awscliv2.zip && \
-     ./aws/install
+     ./aws/install &&
+     rm -r aws
 
 # minio client for S3 use
 curl https://dl.min.io/client/mc/release/linux-amd64/mc -o /usr/local/bin/mc && chmod +x /usr/local/bin/mc
