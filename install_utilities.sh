117f1cd02bd05c739709c091f5e7e91085358df4
Carl Boettiger <cboettig@gmail.com>
2025-09-15 23:01:49 +0000

    uv install

--- install_utilities.sh
+++ install_utilities.sh
@@ -13,7 +13,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
      unzip awscliv2.zip && \
      ./aws/install
 
-# tidy up
+# tidy up aws install
 rm -rf aws/
 rm -r awscliv2.zip
 
@@ -21,11 +21,19 @@ rm -r awscliv2.zip
 curl https://dl.min.io/client/mc/release/linux-amd64/mc -o /usr/local/bin/mc && chmod +x /usr/local/bin/mc
 
 # optional git config
-# git config --system pull.rebase false && \
+git config --system pull.rebase true && \
 # git config --system credential.credentialStore cache && \
 # git config --system credential.cacheOptions "--timeout 30000" && \
-# echo '"\e[5~": history-search-backward' >> /etc/inputrc && \
-# echo '"\e[6~": history-search-forward' >> /etc/inputrc
+
+git config --system user.name "Default User"
+git config --system user.email "email@example.com" 
+
+# uv install
+curl -LsSf https://astral.sh/uv/install.sh | UV_INSTALL_DIR="/usr/local/bin" sh
+
+## terminal preferences
+echo '"\e[5~": history-search-backward' >> /etc/inputrc && \
+echo '"\e[6~": history-search-forward' >> /etc/inputrc
 
 # Optional credential manager for RStudio/Jupyter users. -- code-server extension provides easier alternative. 
 # wget https://github.com/git-ecosystem/git-credential-manager/releases/download/v2.6.0/gcm-linux_amd64.2.6.0.deb && dpkg -i gcm-*.deb && rm gcm-*.deb
