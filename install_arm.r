dd0c9719688271c0e1b41f73187e02ae40a53dbf
Carl Boettiger <cboettig@gmail.com>
2025-10-21 09:41:59 -0700

    use the right repos! :rocket:

--- install_arm.r
+++ install_arm.r
@@ -1,27 +1,7 @@
-#install.packages("pak")
-
-#pak::pak_install
-
 install.packages(c(
-'archive',
-'languageserver',
-'httpgd',
-'quarto',
-'dplyr',
-'purrr',
-'httr2',
-'tidyr',
-'forcats',
-'devtools',
-'reticulate',
-'shiny',
-'duckdbfs',
-'sf',
-'terra',
-'gdalcubes',
-'arrow',
-'V8'))
+    'sf',
+    'gdalcubes',
+    'terra',
+    'duckdbfs'))
 
-install.packages("pak")
-pak::pkg_install("ggplot2") # r-cran-s7 dependency is messed up?
-pak::pkg_install("tidyverse")
+
